d1b60a5e35ba4ff4da7e7cf1a542c7af4906c1ff
Mads Kristensen <post@madskristensen.net>
2016-05-22 12:29:57 -0700

    Added missing unit tests

--- test/ImageSpritesTest/GenerateTest.cs
+++ test/ImageSpritesTest/GenerateTest.cs
@@ -11,34 +11,41 @@ namespace ImageSpritesTest
     public class GenerateTest
     {
         private string _artifacts;
+        private SpriteGenerator _generator;
 
         [TestInitialize]
         public void Setup()
         {
             _artifacts = new DirectoryInfo(@"..\..\Artifacts").FullName;
-
+            _generator = new SpriteGenerator();
         }
 
         [TestMethod]
         public async Task Png96Dpi()
         {
             var fileName = Path.Combine(_artifacts, "png96.sprite");
-            var outFile = Path.ChangeExtension(fileName, ".jpg");
+            var imgFile = Path.ChangeExtension(fileName, ".jpg");
+            var lessFile = Path.ChangeExtension(fileName, ".less");
 
             try
             {
                 var doc = await SpriteDocument.FromFile(fileName);
-                var fragments = new SpriteGenerator().Generate(doc);
+                await _generator.Generate(doc);
 
-                using (var image = Image.FromFile(outFile))
+                using (var image = Image.FromFile(imgFile))
                 {
                     Assert.AreEqual(56, image.Height); // 16 + padding
                     Assert.AreEqual(236, image.Width); // 16 * 6 + padding
                 }
+
+                string less = File.ReadAllText(lessFile);
+                Assert.IsTrue(less.Contains(".sprite-a()"), "Sprite \"a.png\" not generated");
+                Assert.IsTrue(less.Contains("url('png96.jpg')"), "Incorrect url value");
             }
             finally
             {
-                File.Delete(outFile);
+                File.Delete(imgFile);
+                File.Delete(lessFile);
             }
         }
     }
